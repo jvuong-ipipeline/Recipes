ee7732d84b5616b4cc313614a19f26785581c65f
Nhan Vuong <jvuong25@gmail.com>
2020-04-22 15:14:59 -0400

    Implemented recipes view and controller.

--- RecipesSPA/App_Start/RouteConfig.cs
+++ RecipesSPA/App_Start/RouteConfig.cs
@@ -15,9 +15,10 @@ namespace RecipesSPA
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Index" }
             );
+     
         }
     }
 }
--- RecipesSPA/Controllers/HomeController.cs
+++ RecipesSPA/Controllers/HomeController.cs
@@ -51,7 +51,7 @@ namespace RecipesSPA.Controllers
             string url = "https://api.spoonacular.com/recipes/search";
 
 
-            WebRequest request = WebRequest.Create(url + "?query=" + searchTerm + "&apiKey=" + API_KEY);
+            WebRequest request = WebRequest.Create(url + "?query=" + searchTerm+ "&number=25" + "&apiKey=" + API_KEY);
             WebResponse response = request.GetResponse();
 
             Stream dataStream = response.GetResponseStream();
@@ -75,9 +75,98 @@ namespace RecipesSPA.Controllers
                 recipes.Add(recipe);
                 
             }
-            
+
+            TempData["Offset"] = 25;
+            TempData["SearchTerm"] = searchTerm;
+            TempData.Keep();
+
             return View(recipes);
         }
 
+        [HttpGet]
+        public ActionResult SearchNext()
+        {
+
+            string url = "https://api.spoonacular.com/recipes/search";
+
+
+            WebRequest request = WebRequest.Create(url + "?query=" + TempData["searchTerm"].ToString() + "&offset=" + TempData["Offset"].ToString() +"&number=25" + "&apiKey=" + API_KEY);
+
+            WebResponse response = request.GetResponse();
+
+            Stream dataStream = response.GetResponseStream();
+            StreamReader sr = new StreamReader(dataStream);
+            String jsonStr = sr.ReadToEnd();
+
+            sr.Close();
+            response.Close();
+
+            JObject jsonObj = JObject.Parse(jsonStr);
+
+            IList<JToken> results = jsonObj["results"].Children().ToList();
+            JToken imgBaseURI = jsonObj["baseUri"].Value<JToken>();
+
+            IList<Recipe> recipes = new List<Recipe>();
+
+            foreach (JToken result in results)
+            {
+                Recipe recipe = result.ToObject<Recipe>();
+                recipe.Image = imgBaseURI.ToString() + recipe.Image;
+                recipes.Add(recipe);
+
+            }
+
+            TempData["Offset"] = (int)TempData["Offset"] + 25;
+            TempData.Keep();
+
+            return View("Search",recipes);
+        }
+
+
+        public ActionResult Recipes()
+        {
+            //On first render, Recipes view page will display random recipes
+            //When the user search for a specific item, HttpPost Search controller will handle request and return the Search view
+
+            string url = "https://api.spoonacular.com/recipes/random";
+
+
+            WebRequest request = WebRequest.Create(url + "?number=25" + "&apiKey=" + API_KEY);
+
+            WebResponse response = request.GetResponse();
+
+            Stream dataStream = response.GetResponseStream();
+            StreamReader sr = new StreamReader(dataStream);
+            String jsonStr = sr.ReadToEnd();
+
+            sr.Close();
+            response.Close();
+
+            JObject jsonObj = JObject.Parse(jsonStr);
+
+            IList<JToken> results = jsonObj["recipes"].Children().ToList();
+
+            IList<Recipe> recipes = new List<Recipe>();
+
+            foreach (JToken result in results)
+            {
+                Recipe recipe = new Recipe();
+                recipe.Id = (int)result["id"];
+                recipe.Title = result["title"].ToString();
+                recipe.ReadyInMinutes = (int)result["readyInMinutes"];
+                recipe.Serving = (int)result["servings"];
+                recipe.SourceUrl = result["sourceUrl"].ToString();
+                recipe.OpenLicense = 0;
+
+                //bug: some response might not have image
+
+                recipe.Image = result["image"].ToString();
+
+                recipes.Add(recipe);
+            }
+            return View(recipes);
+        }
+        
     }
+   
 }
